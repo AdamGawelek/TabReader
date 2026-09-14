9b482c56dfb45fc60a6443fade38db912379c708
AdamGawelek <gawelek_adam_12507421u@zs2.ostrzeszow.pl>
2026-09-14 12:13:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+// Zdefiniowanie wartosci
+
 let defaultBPM = 120;
 const main = document.getElementById('alphaTab');
 const songSelector = document.getElementById('songSelect')';
 const trackSelector = document.getElementById('trackSelect');
 const bpmSelector = document.getElementById('bpmSelect');
 const PPButton = document.getElementById('playbutton');
+
+// Zdeklarowanie AlphaTab API
+
+const api = new.AlphaTab
